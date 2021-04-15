99adf57b763eec08c9223892e2c600523ed895db
Yash Patni <yash.s.patni@gmail.com>
2021-04-15 11:38:41 +0530

    Day 12

---
+++ 86_Partition_List.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode *before, *after;
+        before = new ListNode();
+        after = new ListNode();
+        ListNode *after_head = after;
+        ListNode *before_head = before;
+        
+        while (head != NULL) {
+            if (head->val < x) {
+                before->next = head;
+                before = before->next;
+            }
+            else {
+                after->next = head;
+                after = after->next;
+            }
+            head = head->next;
+        }
+        
+        after->next = NULL;
+        before->next = after_head->next;
+        
+        return before_head->next;
+    }
+};
