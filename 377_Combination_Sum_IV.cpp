cac04ea6848ef011b973c6bbd47db6f90fb679df
Yash Patni <yash.s.patni@gmail.com>
2021-04-20 10:55:30 +0530

    Day 17

---
+++ 377_Combination_Sum_IV.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<unsigned int> result(target + 1);
+        result[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int num : nums) {
+                if (num <= i) result[i] += result[i - num];
+            }
+        }
+        
+        return result[target];
+    }
+};
